47d6faca9772727a65b52fa50cd3e740108115cd
1clickmake <hades708@naver.com>
2025-06-25 11:11:10 +0900

    수정한 파일 업로드

--- www/wwwroot/php7.cmake.work/template/community/default/head.php
+++ www/wwwroot/php7.cmake.work/template/community/default/head.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined('_CMBOARD_')) exit; // 개별 페이지 접근 불가
 include_once CM_TEMPLATE_PATH.'/lib/menu.lib.php';
-include_once(CM_TEMPLATE_PATH.'/sidemenu.php'); 
 ?>
 <header>
     헤더영역입니다.
